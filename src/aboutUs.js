85b73dcb93ead5adbf7d038c9b0145054b2d1ad1
mcalijr <martink.moc@gmail.com>
2021-11-04 12:10:42 +0100

    Finished site and logic to make it generate using JS. All that's left is splitting it to outer modules and then use webpack to put it together again

---
+++ src/aboutUs.js
@@ -0,0 +1,28 @@
+// about us page
+
+function generateAboutUs() {
+  const switchableContent = document.querySelector(".switchable-content");
+
+  switchableContent.innerHTML = "";
+  const about = document.createElement("div");
+  about.classList.add("about");
+
+  const telephone = document.createElement("div");
+  telephone.classList.add("telephone");
+  telephone.innerHTML = "📞 723 567 373";
+
+  const location = document.createElement("div");
+  location.classList.add("location");
+  location.innerHTML = "🏠 Sezamkowa 29 Street, Warsaw";
+
+  const imgMaps = document.createElement("img");
+  imgMaps.src = "/src/maps.png";
+
+  about.appendChild(telephone);
+  about.appendChild(location);
+  about.appendChild(imgMaps);
+
+  switchableContent.appendChild(about);
+}
+
+export { generateAboutUs };
